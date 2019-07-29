cbb1897cfcc0d1f61b125cb5f8b1f2838933e0d5
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-07-29 16:53:22 -0400

    displaypokemon

---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,4 @@
+export class pokemon{
+    name: string;
+    imagepath: string;
+}
--- src/app/papi.service.ts
+++ src/app/papi.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient }    from '@angular/common/http';
-import{pokedetail} from './models/pokedetail';
+import{ pokedetail } from './models/pokedetail';
+import { pokemon } from './models/pokemon';
+import 'rxjs';
+import { ConsoleReporter } from 'jasmine';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +17,9 @@ pokedetailvar: pokedetail = {
   ability2 : "",
   type : ""
 };
-
+pokemonvar: pokemon[] = [];
+blankpokevar: [];
   constructor(private http: HttpClient) {
-
    }
    getDetailPokemon(): pokedetail{
      var pokemon = "mudkip";
@@ -31,5 +34,15 @@ pokedetailvar: pokedetail = {
      
     return this.pokedetailvar;
      
+   }
+   getAllPokemon(): []{
+    var req:any[];
+    var res = this.http.get<pokemon[]>(`${this.url}pokemon`)
+    .subscribe(poke=> {
+      this.blankpokevar = poke.results;
+      console.log(this.blankpokevar);
+      return this.blankpokevar
+    });
+    return this.blankpokevar;
    }
 }
