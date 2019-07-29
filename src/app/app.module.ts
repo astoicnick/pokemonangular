e78fb5a05a5139e821b41d827f7847eb89858361
Nicholas Perry <nick@Nicholass-MacBook-Pro.local>
2019-07-29 14:19:16 -0400

    Added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemondetailComponent } from './pokemondetail/pokemondetail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { matImports } from './matImports';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { PokemondetailComponent } from './pokemondetail/pokemondetail.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    matImports
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/matImports.ts
@@ -0,0 +1,17 @@
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatSidenavModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { NgModule } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule, MatSelect} from '@angular/material/select';
+import {TextFieldModule} from '@angular/cdk/text-field'; 
+
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatCardModule, MatListModule, MatChipsModule, MatFormFieldModule, MatInputModule, MatSelectModule, TextFieldModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatCardModule, MatListModule, MatChipsModule, MatFormFieldModule, MatInputModule, MatSelectModule,TextFieldModule],
+})
+
+export class matImports { }
