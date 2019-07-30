76a19b57770e7b02dc9129484cdd977f42806e92
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-07-29 21:07:56 -0400

    styling to products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+<<<<<<< Updated upstream
 
 
 const routes: Routes = [];
+=======
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { EvolutionComponent } from './evolution/evolution.component';
+import { PokemondetailComponent } from './pokemondetail/pokemondetail.component';
+
+
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'evolution', component: EvolutionComponent},
+  {path: 'detail', component: PokemondetailComponent}
+];
+>>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -15,7 +15,22 @@ blankpokevar: [];
   constructor(private PokemonService: PapiService) { }  
 
   ngOnInit() {
+<<<<<<< Updated upstream
   console.log(this.PokemonService.getAllPokemon())
+=======
+    this.imageArray = [];
+    this.pokemonList = [];
+    console.log('hey');
+    this.getPokemon();
+  }
+  getPokemon() {
+    var blank: [];
+    this.PokemonService.getAllPokemon()
+    .subscribe(p => { 
+      this.pokemonList = p.results;
+      this.getUrls();
+    });
+>>>>>>> Stashed changes
   }
 }
 
